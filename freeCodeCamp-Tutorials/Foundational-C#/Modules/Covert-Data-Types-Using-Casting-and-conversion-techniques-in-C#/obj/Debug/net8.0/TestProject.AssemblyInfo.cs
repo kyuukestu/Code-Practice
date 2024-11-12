f76fed385db7fc43eaeb6a02a61191ea6389ec18
CloudySkyLoftyMoon <jonariluec@stu.ncu.edu.jm>
2024-11-12 16:49:51 -0500

    .sln fix

--- freeCodeCamp-Tutorials/Foundational-C#/Modules/Covert-Data-Types-Using-Casting-and-conversion-techniques-in-C#/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ freeCodeCamp-Tutorials/Foundational-C#/Modules/Covert-Data-Types-Using-Casting-and-conversion-techniques-in-C#/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd4d81cef490dd7da02d1e2c8dce348789349de6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e08fb8029630ef95b57be2c22c45b116a5ea45a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
