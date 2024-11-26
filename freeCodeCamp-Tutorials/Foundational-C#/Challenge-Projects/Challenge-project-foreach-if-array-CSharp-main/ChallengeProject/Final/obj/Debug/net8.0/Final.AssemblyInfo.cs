a5bd3fe6a7e613265a7a1a3df8fac75c8095bfe4
CloudySkyLoftyMoon <jonariluec@stu.ncu.edu.jm>
2024-11-26 14:07:52 -0500

    Challenge Project: Mini-game

--- freeCodeCamp-Tutorials/Foundational-C#/Challenge-Projects/Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
+++ freeCodeCamp-Tutorials/Foundational-C#/Challenge-Projects/Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/Final/obj/Debug/net8.0/Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50c39b3a4d9f05925e86c204e5a91686e486ac65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+944631ad0328de2d681f00c359dd703be399080e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
