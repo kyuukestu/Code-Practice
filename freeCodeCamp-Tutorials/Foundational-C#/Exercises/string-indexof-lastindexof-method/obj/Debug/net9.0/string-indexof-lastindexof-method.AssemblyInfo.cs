5942936db76fa5b870863260cf5ea3f46c124d4a
CloudySkyLoftyMoon <jonariluec@stu.ncu.edu.jm>
2024-12-03 22:23:25 -0500

    Guided Project: Debugging try-catch.

--- freeCodeCamp-Tutorials/Foundational-C#/Exercises/string-indexof-lastindexof-method/obj/Debug/net9.0/string-indexof-lastindexof-method.AssemblyInfo.cs
+++ freeCodeCamp-Tutorials/Foundational-C#/Exercises/string-indexof-lastindexof-method/obj/Debug/net9.0/string-indexof-lastindexof-method.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("string-indexof-lastindexof-method")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bef24a9655dcf000c89aa21336ebbe1f6a2acba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9080753ec99bda6da942371a3d6cb9f19ba389aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("string-indexof-lastindexof-method")]
 [assembly: System.Reflection.AssemblyTitleAttribute("string-indexof-lastindexof-method")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
